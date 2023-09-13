85f5dbbf066b1ce82b7ae7e248b5ebabdae464c1
Hrudai Thungathurthi <hrudai@hrudai.com>
2023-09-13 13:29:52 -0700

    finder

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import EmailValidator from 'email-deep-validator'
 import express from 'express';
+import permute from 'email-permutator'
 
 const app = express();
 const emailValidator = new EmailValidator();
@@ -19,3 +20,26 @@ app.get('/', async (req,res)=>{
     }
     res.send({"valid":list});
 });
+
+app.get('/finder', async(req,res)=>{
+    const firstName = req.query.firstName;
+    const lastName = req.query.lastName;
+    const domain = req.query.domain;
+    const elist = permute({    
+        firstName:firstName,
+        lastName:lastName,
+        nickName:'',
+        middleName:'',
+        domain1:domain,
+        domain2:'',
+        domain3:'',
+      });
+    const list = []
+    for(const email of elist){
+        const {wellFormed, validDomain, validMailbox } = await emailValidator.verify(email);
+        if(validMailbox===true){
+            list.push(email)
+        }
+    }
+    return({data:list})
+})
