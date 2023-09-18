ee96feeb1caa531a818030fc74a29e906ace2bd9
Hrudai Thungathurthi <hrudai@hrudai.com>
2023-09-18 16:18:45 -0700

    the google search addition for if its not a catchall

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import EmailValidator from 'email-deep-validator'
 import express from 'express';
 import permute from 'email-permutator'
+import google from 'googlethis';
 
 const app = express();
 const emailValidator = new EmailValidator();
@@ -20,7 +21,15 @@ app.get('/', async (req,res)=>{
     }
     res.send({"valid":list});
 });
-
+const options = {
+    page: 0, 
+    safe: false, // Safe Search
+    parse_ads: false, // If set to true sponsored results will be parsed
+    additional_params: { 
+      // add additional parameters here, see https://moz.com/blog/the-ultimate-guide-to-the-google-search-parameters and https://www.seoquake.com/blog/google-search-param/
+      hl: 'en' 
+    }
+  }
 app.get('/finder', async(req,res)=>{
     const startTime = Date.now();
     {
@@ -39,14 +48,23 @@ app.get('/finder', async(req,res)=>{
         domain3:'',
       });
     const list = []
-    const promises = elist.map(async (email) => {
+    let promises = elist.map(async (email) => {
         const {validMailbox } = await emailValidator.verify(email);
         if(validMailbox===true){
             list.push(email)
         }
     });
     await Promise.all(promises)
-    res.send({'data':list,"catchall":false})
+    promises = list.map(async(email)=>{
+        const response = await google.search('"'+email+'"', options);
+        const temp = []
+        temp.push(email)
+        for (let i = 0; i < response.results.length; i++) {
+            temp.push(response.results[i].url);
+        }
+        return temp;
+    })
+    res.send({'data':await Promise.all(promises),"catchall":false})
     const endTime = Date.now(); // Record the end time
     const elapsedTime = endTime - startTime; // Calculate elapsed time
     console.log(`Time taken: ${elapsedTime}ms`);
